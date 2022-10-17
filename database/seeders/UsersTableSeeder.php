7ca208b7bd1e588d15c2c45c3fd064c2448e5a7d
u-shunpei <s.urayama0601@gmail.com>
2022-10-17 20:55:03 +0900

    認証機能実装

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'testuser',
+            'email' => 'testuser@example.com',
+            'password' => bcrypt('testuser0123'),
+            'created_at' =>Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
